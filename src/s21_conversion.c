3d9ceccab3f1f3886048e953e29f4003ebb1f2aa
Sonja David <sonjadav@oa-h1.msk.21-school.ru>
2022-07-20 21:16:12 +0300

    dec to int

--- src/s21_conversion.c
+++ src/s21_conversion.c
@@ -11,3 +11,26 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
     }
     return out;
 }
+
+
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+    int flag = 0;
+    char* tmp;
+   for (int i = 32; i < 96; i++) {
+        if (get_bit(src, i)) {
+            printf("huge val\n");
+            return 1;
+            flag = 1;
+            break;
+            }
+        }
+    
+    if (flag == 0)
+        tmp = dec_to_str(src);
+        *dst = strtof(tmp, NULL);
+/*        if (get_bit(src, 127)) {
+            (*dst) *= 1;
+            printf("catch sign\n");
+            }*/
+    return 0;
+}
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -46,7 +46,7 @@ typedef struct
 
     // int s21_from_int_to_decimal(int src, s21_decimal *dst);
     // int s21_from_float_to_decimal(float src, s21_decimal *dst);
-    // int s21_from_decimal_to_int(s21_decimal src, int *dst);
+    int s21_from_decimal_to_int(s21_decimal src, int *dst);
     int s21_from_decimal_to_float(s21_decimal src, float *dst);
 
 // Другие функции. Возвращают: 0 - OK, 1 - ошибка вычисления
