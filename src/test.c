82d068022fe6da61786568086a6fd5e9dad74431
Vital <govorirk@gmail.com>
2022-07-28 01:40:42 +0300

    fuh

--- src/test.c
+++ src/test.c
@@ -19,12 +19,41 @@ int main() {
 
     s21_decimal dec2 = {{0, 0, MAX_INT, 0}};
     put_exp(&dec2, 25);
-    put_bit(&dec2, 127, 1);
+    put_bit(&dec2, 127, 0);
     
     // s21_decimal dec3 = {{0, 0, 0, 0}};
     s21_decimal dec4 = {{0, 0, 0, 0}};
 
+    s21_decimal dec14 = DEC_TEN;
+    for (int i = 1; i < 14; i++) {
+        s21_mul_simple(dec14, DEC_TEN, &dec14);
+    }
+
+    s21_decimal deca1 = {{0, 0, 0, 0}};
+    s21_decimal deca2 = {{0, 0, 0, 0}};
+    s21_decimal decb1 = {{0, 0, 0, 0}};
+    s21_decimal decb2 = {{0, 0, 0, 0}};
+    s21_decimal tmp = {{0, 0, 0, 0}};
+    s21_decimal res = {{0, 0, 0, 0}};
+
     printf("MID %d\n", s21_mul(dec1, dec2, &dec4));
+    deca2 = s21_div_simple(dec1, dec14, &deca1);
+    decb2 = s21_div_simple(dec2, dec14, &decb1);
+    int exp1 = get_exp(dec1);
+    int exp2 = get_exp(dec2);
+    put_exp(&deca1, exp1 - 14);
+    put_exp(&decb1, exp2 - 14);
+    put_exp(&deca2, exp1);
+    put_exp(&decb2, exp2);
+
+    s21_mul(deca2, decb2, &res);
+    s21_mul(deca1, decb2, &tmp);
+    s21_add(res, tmp, &res);
+    s21_mul(deca2, decb1, &tmp);
+    s21_add(res, tmp, &res);
+    s21_mul(deca1, decb1, &tmp);
+    s21_add(res, tmp, &res);
+
 
     print_bits(dec1);
     print_bits(dec2);
@@ -39,13 +68,16 @@ int main() {
     printf("dec2 || %s\n", f2);
     free(f2);
 
-    // char* f3 = dec_to_str(dec3);
-    // printf("res1 || %s\n", f3);
-    // free(f3);
+    char* f3 = dec_to_str(dec4);
+    printf("MUL  || %s\n", f3);
+    free(f3);
 
-    char* f4 = dec_to_str(dec4);
-    printf("%s\n", f4);
-    free(f4);
+    // char* f4 = dec_to_str(decb2);
+    // printf("res1 || %s\n", f4);
+    // free(f4);
 
+    char* f5 = dec_to_str(res);
+    printf("%s\n", f5);
+    free(f5);
     return 0;
 }
