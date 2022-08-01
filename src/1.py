1fe1554320b6fed345d5f874ae3d7ffba46c1f0d
shifter <vanfre93@gmail.com>
2022-08-01 18:03:15 +0300

    remove useless files

--- src/1.py
+++
@@ -1,6 +0,0 @@
-from decimal import getcontext, Decimal
-
-getcontext().prec = 29
-number = Decimal("0.0000000000000000000000000001") / Decimal("10")
-number = number.quantize(Decimal(".0000000000000000000000000000"))
-print('{:f}'.format(number))
--- src/tests/test/mass_tests/conv.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '../../')
-from py_test import MyDecimal
-
-decimal = MyDecimal(input().strip('\" '))
-
-print("\tEXPONENT:\t\t\t", decimal.exponent)
-print(f"\tFINALY:\t\t\t\t{decimal.decimal:.28f}")
-# print()
