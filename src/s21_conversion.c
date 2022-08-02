b0150adb85a687596b997b29d907bfcaaa4a219a
Sonja David <sonjadav@mi-j4.msk.21-school.ru>
2022-08-02 17:16:30 +0300

    fix fTd

--- src/s21_conversion.c
+++ src/s21_conversion.c
@@ -36,36 +36,46 @@ return out;
 
 
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+    int res = 0;
     int scale_f = scale(src);
-    int decimal_exp = 0;
-    int bit_count = 31;
-    unsigned int fbits = *((unsigned int*)&src);
-    if (!!(0x80000000 & fbits))
-        put_bit(dst, 127, 1);
-    fbits <<= 8;
+    if (abs(scale_f) <= 95) {
+        int decimal_exp = 0;
+        int bit_count = 31;
+        unsigned int fbits = *((unsigned int*)&src);
+        if (!!(0x80000000 & fbits))
+            put_bit(dst, 127, 1);
+        fbits <<= 8;
 
-    for (unsigned int mask = 0x80000000, i = 23; mask; mask >>= 1, i--) {
-        if (i == 23) {
-            put_bit(dst, i, 1);
-        } else if (!!(fbits&mask)) { 
-            put_bit(dst, i, 1);
-        } else {
-            put_bit(dst, i, 0); }
-    }
-    
-    while(!get_bit(*dst, bit_count))
-        bit_count--;
-
-    for (int i = 0; i < bit_count - scale_f; i++) {
-        s21_mul(*dst, DEC_TEN, dst);
-        if(!get_bit(*dst, 0))
-            shift_right(dst);
-        decimal_exp++;
+        for (unsigned int mask = 0x80000000, i = 23; mask; mask >>= 1, i--) {
+            if (i == 23) {
+                put_bit(dst, i, 1);
+            } else if (!!(fbits&mask)) { 
+                put_bit(dst, i, 1);
+            } else {
+                put_bit(dst, i, 0); }
         }
-    
-    put_exp(dst, decimal_exp);
 
+        if (scale_f > 23) {
+            for(int i = scale_f - 23; i > 0; i--)
+                shift_left(dst);
+        } else { 
+            for (int mask = 0b1, i = 23; !(mask & dst->bits[0]) && i > scale_f ; dst->bits[0] >>= 1) 
+                i--;
+        }
+        
+        while(!get_bit(*dst, bit_count))
+            bit_count--;
 
-return 0;
+        for (int i = 0; i < bit_count - scale_f; i++) {
+            s21_mul(*dst, DEC_TEN, dst);
+            if(!get_bit(*dst, 0))
+                shift_right(dst);
+            decimal_exp++;
+            }
+        
+        put_exp(dst, decimal_exp);
+    } else { res = 1; }
+
+return res;
 }
 
