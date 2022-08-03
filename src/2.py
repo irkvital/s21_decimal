956c7d49999c091be870ad5d48c2ae91d1ceea92
Bouvier Rosalva <bouvierr@mi-j2.msk.21-school.ru>
2022-08-03 18:47:03 +0300

    start to autotest

--- src/2.py
+++ src/2.py
@@ -1,7 +1,6 @@
 from decimal import getcontext, Decimal
-import sys
 
 getcontext().prec = 29
-number = Decimal(sys.argv[1]) / Decimal(sys.argv[2])
+number = Decimal("-34408609108464854731690166446") / Decimal("-14.82910912776342005")
 #number = number.quantize(Decimal(".00000000000000000000000000000"))
-print('{:.{}f}'.format(number, sys.argv[3]))
+print('{:.{}f}'.format(number, 28))
--- src/test.c
+++ src/test.c
@@ -1,49 +1,77 @@
-#include <stdio.h>
-#include <math.h>
 #include "./s21_decimal.h"
+#include <time.h>
 
+int rand_dec(s21_decimal* dec);
 
 int main() {
+    srand(time(NULL));
     s21_decimal dec1 = DEC_NUL;
-    char strinp1[32] = "0.1";
-    str_to_dec(strinp1, &dec1);
-    // float f = 533834287383478.666666666666;
-    // s21_from_float_to_decimal(f, &dec1);
+    s21_decimal dec2 = DEC_NUL;
+    s21_decimal dec_res = DEC_NUL;
 
-    // s21_decimal dec2 = DEC_NUL;
-    // char strinp2[32] = "71780112248297297370296977550";
-    // str_to_dec(strinp2, &dec2);
+    rand_dec(&dec1);
+    rand_dec(&dec2);
 
-    // s21_decimal dec3 = {{0, 0, 0, 0}};
-    s21_decimal dec4 = {{0, 0, 0, 0}};
+    s21_div(dec1, dec2, &dec_res);
+    int exp = get_exp(dec_res);
 
-    // printf("MID %d\n", s21_mul(dec1, dec2, &dec4));
-    // printf("MID %d\n", s21_mul(dec1, dec2, &dec4));
+    char* strdec1 = dec_to_str(dec1);
+    char* strdec2 = dec_to_str(dec2);
+
+    char str[120];
+    sprintf(str, "python3 1.py %s %s %d > 1.txt", strdec1, strdec2, exp);
+    system(str);
+
+    char res_py[32];
+    FILE *fp;
+    if (fp = fopen("1.txt", "r")) {
+        fgets(res_py, 32, fp);
+        fclose(fp);
+    } 
+
+// PRINT
+    char* f1 = dec_to_str(dec1);
+    printf("dec1   || %s\n", f1);
+    free(f1);
+    char* f2 = dec_to_str(dec2);
+    printf("dec2   || %s\n", f2);
+    free(f2);
+    char* f3 = dec_to_str(dec_res);
+    printf("21_res || %s\n", f3);
+    free(f3);
+    printf("py_res || %s\n", res_py);
+
+
+
+
+    free(strdec1);
+    free(strdec2);
+
+    // char strinp1[32] = "0.1";
+    // char strinp2[32] = "0.3";
+    // int num = 28;
+    // char str[120];
+    // sprintf(str, "python3 1.py %s %s %d", strinp1, strinp2, num);
+    // system(str);
 
     // print_bits(dec1);
-    // print_bits(dec2);
-    // print_bits(dec3);
-    // print_bits(dec4);
 
-    // printf("FLOAT   %f\n", f);
-    char* f1 = dec_to_str(dec1);
-    printf("dec1 || %s\n", f1);
-    free(f1);
-
-    s21_floor(dec1, &dec4);
-
-    // char* f2 = dec_to_str(dec2);
-    // printf("dec2 || %s\n", f2);
-    // free(f2);
-
-    // char* f3 = dec_to_str(dec3);
-    // printf("DEC3  || %s\n", f3);
-    // free(f3);
-    // print_bits(dec3);
-
-    char* f4 = dec_to_str(dec4);
-    printf("FLOOR|| %s    \n", f4);
-    free(f4);
+    // char* f1 = dec_to_str(dec1);
+    // printf("dec1 || %s\n", f1);
+    // free(f1);
 
     return 0;
+}
+
+int rand_dec(s21_decimal* dec) {
+    int exp = rand() % 29;
+    int sign = rand() % 2;
+    int max = rand() % 3;
+    *dec = DEC_NUL;
+    for (int i = 0; i <= max; i++) {
+        dec->bits[i] = rand() % MAX_INT;
+    }
+    put_bit(dec, 127, sign);
+    put_exp(dec, exp);
+    return exp;
 }
