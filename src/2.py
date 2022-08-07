67163cc921ea48fefac162e3640a631af051bb04
Sonja David <sonjadav@oa-h2.msk.21-school.ru>
2022-08-07 16:19:13 +0300

    rem

--- src/2.py
+++
@@ -1,6 +0,0 @@
-from decimal import getcontext, Decimal
-
-getcontext().prec = 29
-number = Decimal("23142.739021437278912921463090") / Decimal("469971034644081275821.9146474")
-#number = number.quantize(Decimal(".00000000000000000000000000000"))
-print('{:.{}f}'.format(number, 28))
